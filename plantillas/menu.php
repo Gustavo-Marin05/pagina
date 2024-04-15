65c76b2a85da4181dfa444de3b54b33d3440a50c
Gustavo-Marin05 <67645041g@gmail.com>
2024-04-15 14:55:07 -0400

    primeros cambios

---
+++ plantillas/menu.php
@@ -0,0 +1,49 @@
+
+
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary navbar-custom fixed-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img src="images/logonue.png" alt="Logo" width="40" height="34"> facultad de medicina</a>
+            
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle"  role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Facultad
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="institucional.php">Institucional</a></li>
+                            <li><a class="dropdown-item" href="#">Autoridades</a></li>
+                            <li><a class="dropdown-item" href="#">Plantel Docente</a></li>
+                            <li><a class="dropdown-item" href="#">Acreditacion</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Carrera
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Calendario Academico</a></li>
+                            <li><a class="dropdown-item" href="#">Videos</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="investigacion.php">Investigación</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Biblioteca</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+    
+
+
