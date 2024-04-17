8dcd1d080833ab763946e2ef5b4d2fa5ee5bd3bb
Gustavo-Marin05 <67645041g@gmail.com>
2024-04-16 23:53:37 -0400

    4to comit se puso y arrego imagenes a investigacion

--- investigacion.php
+++ investigacion.php
@@ -1,59 +1,58 @@
+<?php
+include("plantillas/inicio.php");
+
+include('plantillas/menu.php');
+?>
 
 
 
-  <?php
-  include ("plantillas/inicio.php");
-  
-  include('plantillas/menu.php');
-  ?>
+<style>
+  .card-img-top {
+    height: 200px;
+    /* Ajusta la altura según tus necesidades */
+    object-fit: cover;
+  }
 
+  .navbar-custom {
+    background-color: #071f69 !important;
+  }
+</style>
 
+<?php
 
-  <style>
-    .card-img-top {
-      height: 200px;
-      /* Ajusta la altura según tus necesidades */
-      object-fit: cover;
-    }
+for ($i = 1; $i <= 10; $i++) {
 
-    .navbar-custom {
-      background-color: #071f69 !important;
-    }
-  </style>
+?>
 
-  <?php
-
-  for ($i = 1; $i <= 10; $i++) {
-
-  ?>
-    <div class="container">
-      <div class="row">
-        <div class="col-12 col-md-6">
-          <div class="card">
-            <img src="https://th.bing.com/th/id/R.0951c5037774db3e27cc342f3fe5df8e?rik=KIH%2faesAV0EE1w&riu=http%3a%2f%2fimg.europapress.net%2ffotoweb%2ffotonoticia_20150921124337_1280.jpg&ehk=swmDx6fPd0NwSryfBWTYTm5hTWkip238sWfOBS2IIEk%3d&risl=&pid=ImgRaw&r=0" class="card-img-top" alt="Investigación 1">
-            <div class="card-body">
-              <h5 class="card-title">Investigación sobre nuevos tratamientos para la diabetes</h5>
-              <p class="card-text">Un equipo de investigadores de la Universidad San Francisco Xavier está desarrollando nuevos enfoques terapéuticos para el tratamiento de la diabetes. Se están explorando estrategias innovadoras para controlar los niveles de glucosa y mejorar la calidad de vida de los pacientes.</p>
-              <a href="url_de_la_pagina_1" class="btn btn-primary">Más información</a> <!-- Reemplaza "url_de_la_pagina_1" con la URL a la que deseas conectar -->
-            </div>
+<br><br><br>
+  <div class="container" style="padding: 10px;">
+    <div class="row">
+      <div class="col-md-6">
+        <div class="card">
+          <img src="images/caru2.jpg" class="card-img-top" alt="Investigación 1">
+          <div class="card-body">
+            <h5 class="card-title">Investigación sobre nuevos tratamientos para la diabetes</h5>
+            <p class="card-text">Un equipo de investigadores de la Universidad San Francisco Xavier está desarrollando nuevos enfoques terapéuticos para el tratamiento de la diabetes. Se están explorando estrategias innovadoras para controlar los niveles de glucosa y mejorar la calidad de vida de los pacientes.</p>
+            <a href="url_de_la_pagina_1" class="btn btn-primary">Más información</a> <!-- Reemplaza "url_de_la_pagina_1" con la URL a la que deseas conectar -->
           </div>
         </div>
-        <div class="col-12 col-md-6">
-          <div class="card">
-            <img src="https://th.bing.com/th/id/R.be770039c957cd523a727ca0e238828b?rik=WXllR%2fUhoT%2fHsg&riu=http%3a%2f%2fwww.hamedanpayam.com%2fgalery%2fnewscontent%2f0xdea168acc81c684c_0.jpg&ehk=BnXw%2b8i%2fSDFMHn%2bSRI4E61zOufeuQCJrqiVuAw4bggc%3d&risl=&pid=ImgRaw&r=0" class="card-img-top" alt="Investigación 2">
-            <div class="card-body">
-              <h5 class="card-title">Desarrollo de terapias avanzadas para enfermedades cardiovasculares</h5>
-              <p class="card-text">Investigadores de la Universidad San Francisco Xavier están liderando proyectos para desarrollar terapias avanzadas para enfermedades cardiovasculares. Se están investigando nuevas formas de prevenir y tratar enfermedades como la hipertensión y la insuficiencia cardíaca.</p>
-              <a href="url_de_la_pagina_2" class="btn btn-primary">Más información</a> <!-- Reemplaza "url_de_la_pagina_2" con la URL a la que deseas conectar -->
-            </div>
+      </div>
+      <div class="col-md-6">
+        <div class="card">
+          <img src="images/cra.jpg" class="card-img-top" alt="Investigación 2">
+          <div class="card-body">
+            <h5 class="card-title">Desarrollo de terapias avanzadas para enfermedades cardiovasculares</h5>
+            <p class="card-text">Investigadores de la Universidad San Francisco Xavier están liderando proyectos para desarrollar terapias avanzadas para enfermedades cardiovasculares. Se están investigando nuevas formas de prevenir y tratar enfermedades como la hipertensión y la insuficiencia cardíaca.</p>
+            <a href="url_de_la_pagina_2" class="btn btn-primary">Más información</a> <!-- Reemplaza "url_de_la_pagina_2" con la URL a la que deseas conectar -->
           </div>
         </div>
       </div>
     </div>
+  </div>
 
-  <?php
+<?php
 
-  }
+}
 
-  include ("plantillas/final.php");
-  ?>
+include("plantillas/final.php");
+?>
